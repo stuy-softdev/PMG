41f3743e8890d4b27589e3fb12e0caa4b0a41765
Ethan <echeung60@stuy.edu>
2026-05-08 11:43:40 -0400

    add wsgi.py

---
+++ app/wsgi.py
@@ -0,0 +1,4 @@
+from __init__ import app
+
+if __name__ == "__main__": # BE CAREFUL WITH THIS. IT MIGHT SAVE AS THOSE EVIL FANCY QUOTES WHICH WILL BREAK IT
+	app.run() # <tab> just represents the tab character, as markdown doesn't let me enter a tab there
