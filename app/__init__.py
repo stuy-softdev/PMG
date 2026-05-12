44f4d1fd83afdf1856faedab3cd459d5fbb25f55
Jason Chan <jchan61@stuy.edu>
2026-05-12 11:41:54 -0400

    i love misspelling git puhs

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,71 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+import sqlite3, datetime   #enable control of an sqlite database
+
+# our helper db files
+import data_setup, data
+
+DB_FILE="data.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+# create tables
+data_setup.create_users_table()
+data_setup.create_game_table()
+data_setup.create_userdata_table()
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+
+    # stored active session, take user to response page
+    if 'username' in session:
+        return redirect(url_for("home"))
+
+    if 'username' in request.form:
+        username = request.form.get('username').strip().lower()
+        password = request.form.get('password').strip()
+
+        # check if password is correct, if not then reload page
+        if not data.auth(username, password):
+            return render_template("login.html", error="Username or password is incorrect")
+
+        # if password is correct redirect home
+        session["username"] = username
+        return redirect(url_for("home"))
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None) # remove username from session
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+
+    if request.method == 'POST':
+        username = request.form.get('username').strip().lower()
+        password = request.form.get('password').strip()
+
+        # reload page if no username or password was entered
+        if not username or not password:
+            return render_template("register.html", error="No username or password inputted")
+
+        # puts user into database unless if there's an error
+        execute_register = data.add_user(username, password)
+        if execute_register == "success":
+            session['username'] = username
+            return redirect(url_for("home"))
+        else:
+            return render_template("register.html", error = execute_register)
+    return render_template("register.html")
 
 if __name__ == "__main__":
+    app.debug=True
     app.run(host='0.0.0.0')
