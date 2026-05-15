5e6913be4f0a15f0f5ae0246a05e32f612ff2cf7
Mycroft Lee <mycroftlee@gmail.com>
2026-05-15 15:47:07 +0000

    Update buzzer.js

---
+++ app/static/buzzer.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let question = "test?" 
+  let letter = ''
+
+  function displayquestion(questionDQ) {
+    while question.length != 0): 
+  }
+                        
