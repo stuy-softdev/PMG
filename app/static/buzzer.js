96657becc44e1d5df34bdd1db32bae4a83500c29
Mycroft Lee <mycroftlee@gmail.com>
2026-05-18 15:48:29 +0000

    making interval for question display

--- app/static/buzzer.js
+++ app/static/buzzer.js
@@ -1,8 +1,31 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let question = "test?" 
-  let letter = ''
+
+  
 
   function displayquestion(questionDQ) {
-    while question.length != 0): 
+
+    let letter = '';
+    let questionDP = '';
+    
+    while (questionDQ.length != 0) {
+      const questionInterval = setInterval(displayquestionIV(questionDQ), 500);
+    }
+    else {
+      clearInterval(questionInterval);
+    }
+    
   }
-                        
+
+
+  function displayquestionIV(questionDQ_IV) {
+
+    letter = questionDQ_IV.slice(0, 1);
+    questionDP =+ letter;
+    questionDQ_IV = questionDQ_IV.replace(letter, "");
+    disp(questionDP);
+    
+  }
+  
+
+  
+}
