6b30c1015eb9af35ae447383eadf5bf8dc672a15
jason chan <jchan@softdevdroplet.(none)>
2026-06-15 07:57:39 +0000

    ihatemyself

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,4 @@
 from __init__ import app
 
-if __name__ == "__main__": # BE CAREFUL WITH THIS. IT MIGHT SAVE AS THOSE EVIL FANCY QUOTES WHICH WILL BREAK IT
-	app.run() # <tab> just represents the tab character, as markdown doesn't let me enter a tab there
+if __name__ == "__main__":
+    socketio.run(app, debug=False, host = '0.0.0.0')
